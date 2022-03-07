31f53ab238f7f90b09c0034bfcfe3b1c9985a571
caparicio <caparicio@esdmadrid.es>
2022-03-07 18:59:07 +0100

    some declarations

--- interface-dashboard/src/app/app.d.ts
+++ interface-dashboard/src/app/app.d.ts
@@ -1,2 +1,18 @@
 declare module "angularjs-dragula";
 declare module "ui-leaflet";
+
+
+type NGSIType = "Number" | "geo:point"
+interface DeviceAttribute {
+    name: string, 
+    device: {
+        id: string, 
+        type: NGSIType, 
+    }, 
+    metadata: {}
+    [key: string]: any // for angular stuff
+}
+interface Sensor {
+    id: string, 
+    attributes: DeviceAttribute[]
+}
